3459770ae9dd9987b08b8aaab08f5d14a55ed448
Vehicola <vehicola@gmail.com>
2017-05-02 19:59:31 -0700

    updates

--- js/init.js
+++ js/init.js
@@ -3,9 +3,12 @@
 
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
+// Show sideNav
+//$('.button-collapse').sideNav('show');
   $('.collapsible').collapsible();
   $('.parallax').parallax();
 
